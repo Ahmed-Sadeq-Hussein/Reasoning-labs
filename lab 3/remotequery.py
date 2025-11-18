a274f50821e2c8ebabc4d0ba60fe69ff59dbdf6d
Baesar <noju22ez@student.ju.se>
2025-11-18 19:01:36 +0100

    added remotequery.py

--- lab 3/remotequery.py
+++ lab 3/remotequery.py
@@ -1,14 +1,17 @@
-#He Tan, 2024-10
+# He Tan, 2024-10
 #
 #
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 
-#connect to the DBpedia SPARQL endpoint
+# Connect to the DBpedia SPARQL endpoint.
 sparql = SPARQLWrapper("https://yago-knowledge.org/sparql/query")
 sparql.setReturnFormat(JSON)
 
-#prepare the query
+# Select the query. [1-4]
+query = 3
+
+
 def prequery(q):
     sparql.setQuery(q)
 
@@ -20,21 +23,62 @@ prefix = """PREFIX schema: <http://schema.org/>
             
             """
 
-query_movie = prefix + """SELECT * WHERE {
-                        ?sub a schema:Movie  .
-                   } 
-                LIMIT 10"""
+query_1_who_influenced_the_beatles = prefix + """SELECT DISTINCT ?artist WHERE {
+                                                ?artist ^schema:influencedBy yago:The_Beatles .
+                                                ?artist a ?class .
+                                                ?class rdfs:subClassOf+ yago:Creator .
+                                            } 
+                                            LIMIT 10"""
 
-prequery(query_movie)
+query_2_influenced_by_the_beatles = prefix + """SELECT DISTINCT ?artist ?nationality WHERE {
+                                                ?artist (schema:influencedBy)+ yago:The_Beatles .
+                                                ?artist a ?class .
+                                                ?class rdfs:subClassOf+ yago:Creator .
+                                                OPTIONAL { ?artist schema:nationality ?nationality . }
+                                                FILTER (?artist != yago:The_Beatles)
+                                            } 
+                                            LIMIT 10"""
+
+query_3_movies_1960_1970 = prefix + """SELECT DISTINCT ?movie ?year ?director  WHERE {
+                                                yago:The_Beatles schema:award/^schema:award ?director .
+                                                ?movie schema:director ?director ;
+                                                            schema:dateCreated ?date
+                                                BIND (year(?date) AS ?year)
+ 											    FILTER (?year >= 1960 && ?year < 1970)
+                                            } 
+                                            LIMIT 10"""
+
+query_4_the_beatles_family_members = prefix + """SELECT ?person WHERE {
+                                                { yago:The_Beatles ^schema:memberOf/schema:children ?person }
+                                                UNION
+                                                { yago:The_Beatles ^schema:memberOf/schema:spouse ?person }
+                                                UNION
+                                                {?person schema:memberOf yago:The_Beatles }
+                                            } 
+                                            LIMIT 10"""
+
+match query:
+    case 1:
+        prequery(query_1_who_influenced_the_beatles)
+        print("Query 1: Everyone who influenced The Beatles.")
+    case 2:
+        prequery(query_2_influenced_by_the_beatles)
+        print("Query 2: Everyone influenced by The Beatles. (This one might take a while)")
+    case 3:
+        prequery(query_3_movies_1960_1970)
+        print("Query 3: Movies (from 1960 to 1970) directed by people who have won the same award as The Beatles.")
+    case _:
+        prequery(query_4_the_beatles_family_members)
+        print("Query 4: Members of The Beatles and their family members.")
 
 try:
     ret = sparql.queryAndConvert()
 
-    print(ret)
-
     for r in ret["results"]["bindings"]:
-        print(r)
-        
+        for var in ret["head"]["vars"]:
+            print(f"{var}: ", r[var]["value"].replace(
+                "http://yago-knowledge.org/resource/", "").replace("_", " "))
+        print("")
+
 except Exception as e:
     print(e)
-
